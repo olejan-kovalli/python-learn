ca88a254ac954ea64de46f6cd53de88d37a3f7e6
Oleg Kovalevsky <kovalevskiol@mail.ru>
2024-08-09 00:25:18 +0300

    ex part 4 start

---
+++ part4/adder_mod.py
@@ -0,0 +1,10 @@
+def adder(a, b):
+    return a + b
+
+adder('abc', 'ced')
+adder([1, 3, 5], [2, 3, 4])
+adder(3.14129268, 2.718)
+
+print(adder('abc', 'ced'))
+print(adder([1, 3, 5], [2, 3, 4]))
+print(adder(3.14129268, 2.718))
